73fd5dcdee76f545c6771111990aa77df06d6cf2
Rochelle Edwards <rochellelynn.programmer@gmail.com>
2021-08-20 00:18:04 -0500

    sorting and presenting data complete

--- README.md
+++ README.md
@@ -1,2 +1,54 @@
 # SmartLogicSkillAssessment
 Skill Assessment completed for SmartLogic Internship Program
+
+Chosen language was Python3.
+
+This script can be ran from the command line.
+
+In order to do so, when inside of the directory in you CLI, input the following arguments:
+
+python assessment.py [list of file names]
+
+The above command will by default sort files by gender, then last name, and print sorted and formatted information into the terminal
+
+Flags for different behavior:
+
+---
+-o or --output followed by 1, 2, or 3
+
+1: Sorts first on gender, then on last name
+
+2: Sorts on date of birth, then last name
+
+3: Sorts last name in descending order
+
+---
+-s or --summary
+
+This command will instruct the script to store the output into OutputSummary.txt
+
+---
+
+Expected formatting for input files:
+
+***
+Pipe Delimited Files
+
+LastName | FirstName | MiddleInitial | Gender | FavoriteColor | DateOfBirth
+
+***
+Comma Delimited Files
+
+LastName, FirstName, Gender, FavoriteColor, DateOfBirth
+
+***
+Space Delimited Files
+
+LastName FirstName MiddleInitial Gender DateOfBirth FavoriteColor
+
+***
+
+
+Expected Format after processing:
+
+LastName FirstName Gender DateOfBirth(M/D/YYYY) FavoriteColor
--- assessment.py
+++ assessment.py
@@ -9,9 +9,49 @@ __author__ = "Rochelle Edwards rochellelynn.programmer@gmail.com"
 import sys
 import argparse
 import re
+from datetime import datetime
+
+
+def ListJoiner(list):
+  """ Takes in a nested list, returns a list of strings """
+  temp_list = []
+
+  for item in list:
+    i = " ".join(item)
+    temp_list.append(i)
+
+  return temp_list
+
+
+def SortGenderThenLast(list):
+  """ Returns a list sorted by gender (females before males),
+  then by last name ascending """
+  sorted_list = sorted(list, key=lambda i: (i[2], i[0]))
+  joined = ListJoiner(sorted_list)
+
+  return joined
+
+
+def SortDobThenLast(list):
+  """ Returns a list sorted by ascending DOB, then by last name ascending"""
+  sorted_list = sorted(list,
+                      key=lambda i: (datetime.strptime(i[3], '%m/%d/%Y'), i[0]))
+  joined = ListJoiner(sorted_list)
+
+  return joined
+
+
+def SortLast(list):
+  """ Returns a list sorted by last name, descending"""
+  sorted_list = sorted(list, key=lambda i: i[0], reverse=True)
+  joined = ListJoiner(sorted_list)
+
+  return joined
+
 
 def PipeFormat(contents):
-  """Doc sting to get filled out"""
+  """Takes in contents of txt file that is delimited with pipes, orders data
+  appropriately, converts gender letters into strings, and returns a list"""
   temp_list = []
   regexp_pipe = r"(\w+)\W+(\w+)\W+\w\W+(\w)\W+(\w+)\W+(\d+/\d+/\d+)"
   reg_return = re.findall(regexp_pipe, contents)
@@ -23,10 +63,13 @@ def PipeFormat(contents):
     else:
       gender = "Female"
     temp_list.append([last, first, gender, dob, color])
+
   return temp_list
 
+
 def CommaFormat(contents):
-  """Doc string to be filled out"""
+  """Takes in contents of txt file that is delimited with commas, orders data
+  appropriately, and returns a list"""
   temp_list = []
   regexp_comma = r"(\w+)\W\s(\w+)\W\s(\w+)\W\s(\w+)\W\s(\d+/\d+/\d+)"
   reg_return = re.findall(regexp_comma, contents)
@@ -34,10 +77,13 @@ def CommaFormat(contents):
   for item in reg_return:
     (last, first, gender, color, dob) = item
     temp_list.append([last, first, gender, dob, color])
+
   return temp_list
 
+
 def SpaceFormat(contents):
-  """Doc string to be filled out"""
+  """Takes in contents of txt file that is delimited with spaces, orders data
+  appropriately, converts gender letters into strings, and returns a list"""
   temp_list = []
   regexp_comma = r"(\w+)\s(\w+)\s\w\s(\w)\s(\d+/\d+/\d+)\s(\w+)"
   reg_return = re.findall(regexp_comma, contents)
@@ -49,11 +95,14 @@ def SpaceFormat(contents):
     else:
       gender = "Female"
     temp_list.append([last, first, gender, dob, color])
+
   return temp_list
 
 
 def CheckDelimiterAndFormat(filename):
-  """doc string to be filled out"""
+  """Takes in a file, runs various reg exps on it to see how it is delimited,
+  calls appropraite format function, returns properly formated list"""
+
   with open(filename, 'r') as f:
     contents = f.read()
 
@@ -73,27 +122,54 @@ def CheckDelimiterAndFormat(filename):
 
 
 def ProcessFiles(file_list, desired_output):
-  """Doc String to be filled out"""
+  """Takes in a file list, calls function to check delimiter and format, extends
+  returned list into a list that needs to be sorted. Once all files are 
+  formatted, lists are sorted based off what the desired output number is. Once
+  list is formatted and sorted, list is returned"""
   list_to_sort = []
+  list_to_return = [f"Output {desired_output}:"]
+
   for file in file_list:
     formated_list = CheckDelimiterAndFormat(file)
     list_to_sort.extend(formated_list)
-  print(list_to_sort)
 
-  print(desired_output)
+  if desired_output == 1:
+    sorted_list = SortGenderThenLast(list_to_sort)
+    list_to_return.extend(sorted_list)
+  elif desired_output == 2:
+    sorted_list = SortDobThenLast(list_to_sort)
+    list_to_return.extend(sorted_list)
+  else:
+    sorted_list = SortLast(list_to_sort)
+    list_to_return.extend(sorted_list)
+
+  return list_to_return
+
+
+def DisplayData(list, summary):
+  """Takes in a formatted and sorted list, and depending on whether or not the
+  summary flag is raise, data is printed to the terminal or saved into 
+  OutputSummary.txt"""
+  if summary == False:
+    for item in list:
+      print(item)
+  else:
+    for item in list:
+      with open('OutputSummary.txt', "a") as f:
+        f.write(f"{item} \n")
+    with open('OutputSummary.txt', "a") as f:
+        f.write("\n")
 
 
 def create_parser():
   """Creates a command line argument parser with one positional argument and 
-  two optional flags with default values.
-  """
+  two optional flags with default values."""
 
   parser = argparse.ArgumentParser(
     description=
     """Extracts data from files and sorts it based of chosen output flag. 
     Once files are sorted and combined into a single list, data is either
-    printed into the terminal or stored into a summary file.
-    """
+    printed into the terminal or stored into a summary file."""
     )
 
   parser.add_argument("files", nargs="+", 
@@ -107,14 +183,18 @@ def create_parser():
 
   parser.add_argument("-s", "--summary", action="store_true", 
   help="""If summary flag is raised, sorted file data will be stored into a new 
-  file with in this directory. If it is not, data will print to the terminal.
-  """
+  file with in this directory. If it is not, data will print to the terminal."""
   )
+
   return parser
 
 
 def main(args):
-  """This Doc string needs to be filled out before turing in"""
+  """Main function of assessment.py. It calls function to create parser, creates
+  the namespace, extracts variables from namespace, notifies user if arguments
+  are not provided in the command line. If CLI arguments meet mandatory 
+  minimum requirements, files get processed and outputted accordingly and
+  presented based of user's choice of summary"""
   parser = create_parser()
   ns = parser.parse_args(args)
 
@@ -126,8 +206,8 @@ def main(args):
   desired_output = int(ns.output)
   summary = ns.summary
 
-  ProcessFiles(file_list = file_list, desired_output = desired_output)
-
+  processed_files = ProcessFiles(file_list, desired_output)
+  DisplayData(processed_files, summary)
 
 
 if __name__ == '__main__':
